5bdba473ae5c519b86913fff98d88533298bc54c
jeongjaein <jaein1608@gmail.com>
2023-12-29 16:29:49 +0900

    Initial commit

---
+++ src/test/java/jaein/spring/SpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package jaein.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
