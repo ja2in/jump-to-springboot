1697be6d64c821c8002a351649ce5442ac5f1195
jeongjaein <jaein1608@gmail.com>
2024-01-09 10:52:10 +0900

    feat: 페이징 테스트를 위해 반복문 추가

--- src/main/java/jaein/spring/InitDb.java
+++ src/main/java/jaein/spring/InitDb.java
@@ -1,6 +1,7 @@
 package jaein.spring;
 
 import jaein.spring.question.Question;
+import jaein.spring.question.QuestionService;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
@@ -27,13 +28,21 @@ public class InitDb {
     static class InitService {
 
         private final EntityManager em;
+        private final QuestionService questionService;
 
         public void dbQuestion() {
-            Question q1 = new Question();
-            q1.setSubject("테스트 질문입니다.");
-            q1.setContent("테스트 내용입니다.");
-            q1.setCreateDate(LocalDateTime.now());
-            em.persist(q1);
+//            Question q1 = new Question();
+//            q1.setSubject("테스트 질문입니다.");
+//            q1.setContent("테스트 내용입니다.");
+//            q1.setCreateDate(LocalDateTime.now());
+//            em.persist(q1);
+
+            //페이징 기능 테스트 하기 위한 코드
+            for(int i = 1; i <= 300; i++){
+                String subject = String.format("테스트 데이터입니다:[%03d]", i);
+                String content = "내용 없음";
+                this.questionService.create(subject, content);
+            }
         }
 
         public void dbAnswer() {
