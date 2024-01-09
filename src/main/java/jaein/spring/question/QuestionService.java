51a2d722d2cb348feddf913771ec62313d694a40
jeongjaein <jaein1608@gmail.com>
2024-01-09 10:54:34 +0900

    feat: 페이지 기능 추가

--- src/main/java/jaein/spring/question/QuestionService.java
+++ src/main/java/jaein/spring/question/QuestionService.java
@@ -2,6 +2,9 @@ package jaein.spring.question;
 
 import jaein.spring.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -13,6 +16,11 @@ import java.util.Optional;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+
+    public Page<Question> getList(int page){
+        Pageable pageable = PageRequest.of(page, 10);
+        return this.questionRepository.findAll(pageable);
+    }
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
