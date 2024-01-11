4b43835e04139a8005d7f888b221042f48de3cda
jeongjaein <jaein1608@gmail.com>
2024-01-11 16:18:55 +0900

    style: 주석처리

--- src/main/java/jaein/spring/InitDb.java
+++ src/main/java/jaein/spring/InitDb.java
@@ -14,40 +14,40 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class InitDb {
 
-    private final InitService initService;
-
-    @PostConstruct
-    public void init() {
-        initService.dbQuestion();
-        initService.dbAnswer();
-    }
-
-    @Component
-    @Transactional
-    @RequiredArgsConstructor
-    static class InitService {
-
-        private final EntityManager em;
-        private final QuestionService questionService;
-
-        public void dbQuestion() {
+//    private final InitService initService;
+//
+//    @PostConstruct
+//    public void init() {
+//        initService.dbQuestion();
+//        initService.dbAnswer();
+//    }
+//
+//    @Component
+//    @Transactional
+//    @RequiredArgsConstructor
+//    static class InitService {
+//
+//        private final EntityManager em;
+//        private final QuestionService questionService;
+//
+//        public void dbQuestion() {
 //            Question q1 = new Question();
 //            q1.setSubject("테스트 질문입니다.");
 //            q1.setContent("테스트 내용입니다.");
 //            q1.setCreateDate(LocalDateTime.now());
 //            em.persist(q1);
-
-            //페이징 기능 테스트 하기 위한 코드
-            for(int i = 1; i <= 300; i++){
-                String subject = String.format("테스트 데이터입니다:[%03d]", i);
-                String content = "내용 없음";
-                this.questionService.create(subject, content);
-            }
-        }
-
-        public void dbAnswer() {
-
-        }
-    }
+//
+//            //페이징 기능 테스트 하기 위한 코드
+//            for(int i = 1; i <= 300; i++){
+//                String subject = String.format("테스트 데이터입니다:[%03d]", i);
+//                String content = "내용 없음";
+//                this.questionService.create(subject, content);
+//            }
+//        }
+//
+//        public void dbAnswer() {
+//
+//        }
+//    }
 
 }
