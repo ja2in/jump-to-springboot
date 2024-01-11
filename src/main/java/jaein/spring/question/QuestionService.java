1297c63bf4d93426000c9fb65d210cfc281d57e3
jeongjaein <jaein1608@gmail.com>
2024-01-11 16:20:11 +0900

    style: exception 위치변경

--- src/main/java/jaein/spring/question/QuestionService.java
+++ src/main/java/jaein/spring/question/QuestionService.java
@@ -1,6 +1,6 @@
 package jaein.spring.question;
 
-import jaein.spring.DataNotFoundException;
+import jaein.spring.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
