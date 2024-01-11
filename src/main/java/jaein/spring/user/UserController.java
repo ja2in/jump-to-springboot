d76f08245092d5ee1dfb6dbfe412481c16fd3569
jeongjaein <jaein1608@gmail.com>
2024-01-11 16:45:16 +0900

    fix: RestController를 RequestMapping으로 수정

--- src/main/java/jaein/spring/user/UserController.java
+++ src/main/java/jaein/spring/user/UserController.java
@@ -6,11 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @Controller
-@RestController("/user")
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
