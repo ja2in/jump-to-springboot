5aceaa589012be975a395a422dc60f2a55e71731
jeongjaein <jaein1608@gmail.com>
2024-01-11 16:21:43 +0900

    feat: UserService 추가

---
+++ src/main/java/jaein/spring/user/UserService.java
@@ -0,0 +1,23 @@
+package jaein.spring.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String email, String password){
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password)); //패스워드 암호화 필수
+        this.userRepository.save(user);
+        return user;
+    }
+}
