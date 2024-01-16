86a3e232386ff086d105cc1a6c79f831e3fad0ac
jeongjaein <jaein1608@gmail.com>
2024-01-16 17:09:23 +0900

    feat: 작성자 속성 추가

--- src/main/java/jaein/spring/question/Question.java
+++ src/main/java/jaein/spring/question/Question.java
@@ -1,6 +1,7 @@
 package jaein.spring.question;
 
 import jaein.spring.answer.Answer;
+import jaein.spring.user.SiteUser;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,4 +26,7 @@ public class Question {
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList;
+
+    @ManyToOne
+    private SiteUser author;
 }
