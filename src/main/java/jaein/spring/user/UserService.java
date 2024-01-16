5abc7c2f872cf3a03d9ad192ceeed27e0fe8e4e0
jeongjaein <jaein1608@gmail.com>
2024-01-16 17:11:18 +0900

    feat: getUser 로직 추

--- src/main/java/jaein/spring/user/UserService.java
+++ src/main/java/jaein/spring/user/UserService.java
@@ -1,9 +1,12 @@
 package jaein.spring.user;
 
+import jaein.spring.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,5 +22,15 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(password)); //패스워드 암호화 필수
         this.userRepository.save(user);
         return user;
+    }
+
+    public  SiteUser getUser(String username){
+        Optional<SiteUser> siteUser = this.userRepository.findByUsername(username);
+
+        if(siteUser.isPresent()){
+            return siteUser.get();
+        } else {
+            throw new DataNotFoundException("siteuser not found");
+        }
     }
 }
