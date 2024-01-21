a24f490e5fe9021b7b5f974a486b2f1677bbdc31
jeongjaein <jaein1608@gmail.com>
2024-01-21 18:06:52 +0900

    feat: 수정 서비스 생성

--- src/main/java/jaein/spring/answer/AnswerService.java
+++ src/main/java/jaein/spring/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package jaein.spring.answer;
 
+import jaein.spring.exception.DataNotFoundException;
 import jaein.spring.question.Question;
 import jaein.spring.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,12 +15,27 @@ public class AnswerService {
 
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content, SiteUser author){
+    public void create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
         answer.setAuthor(author);
+        this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content){
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
 }
