22a129c45b9913bb310cb7c62bc9649df5574376
jeongjaein <jaein1608@gmail.com>
2024-01-22 17:52:23 +0900

    feat: 추천 기능 추가

--- src/main/java/jaein/spring/answer/Answer.java
+++ src/main/java/jaein/spring/answer/Answer.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Getter @Setter
@@ -27,4 +28,7 @@ public class Answer {
     private SiteUser author;
 
     private LocalDateTime modifyDate;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
--- src/main/java/jaein/spring/question/Question.java
+++ src/main/java/jaein/spring/question/Question.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter @Setter
@@ -31,4 +32,7 @@ public class Question {
     private SiteUser author;
 
     private LocalDateTime modifyDate;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
